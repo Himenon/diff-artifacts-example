d96f6b11bd29cd9f862d5b16df39acbf068a2575
K.Himeno <6715229+Himenon@users.noreply.github.com>
2026-01-19 16:01:40 +0900

    fix: path

--- apps/nextjs16-project/next.config.ts
+++ apps/nextjs16-project/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   generateBuildId: async () => {
     return process.env.CI_BUILD_ID || null;
   },
+  experimental: {
+    cssChunking: false,
+    optimizeCss: false,
+  },
   turbopack: {
     debugIds: true
   }
