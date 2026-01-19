9107bdd371f720921e6599d4d6ddca8ceac4e23c
K.Himeno <6715229+Himenon@users.noreply.github.com>
2026-01-19 15:55:25 +0900

    fix: command

--- apps/nextjs16-project/next.config.ts
+++ apps/nextjs16-project/next.config.ts
@@ -2,21 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   generateBuildId: async () => {
-    return process.env.CI_BUILD_ID || "fixed-build-id";
+    return process.env.CI_BUILD_ID || null;
   },
-  turbopack: {},
-  webpack: (config, { dev }) => {
-    if (process.env.CI_BUILD_ID) {
-      config.optimization.chunkIds = 'deterministic';
-      config.optimization.moduleIds = 'deterministic';
-
-      // ハッシュ関数とハッシュの長さを固定化
-      config.output.hashFunction = 'xxhash64';
-      config.output.hashDigest = 'hex';
-      config.output.hashDigestLength = 8;
-    }
-    return config;
-  },
+  turbopack: {
+    debugIds: true
+  }
 };
 
 export default nextConfig;
