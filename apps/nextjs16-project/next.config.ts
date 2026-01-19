5ddea1be2f06a0efdf3e746b5bde1b9e46f7e11f
K.Himeno <6715229+Himenon@users.noreply.github.com>
2026-01-19 15:33:05 +0900

    fix: chunk

--- apps/nextjs16-project/next.config.ts
+++ apps/nextjs16-project/next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   generateBuildId: async () => {
-    return process.env.CI_BUILD_ID || null;
+    return process.env.CI_BUILD_ID || "fixed-build-id";
   },
+  turbopack: {},
   webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer && process.env.CI_BUILD_ID) {
+    if (!dev && !isServer) {
       config.optimization.chunkIds = 'deterministic';
+      config.optimization.moduleIds = 'deterministic';
     }
     return config;
   },
